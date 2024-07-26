a0cb9adf8589c53e5c326e098b47820c16494230
astrid <git@astrid.email>
2024-07-26 15:04:11 +0200

    fixes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { Sheet } from '@grand-hawk/ui-components';
+import { Sheet, Typography } from '@grand-hawk/ui-components';
 import React from '@rbxts/react';
 import { createPortal } from '@rbxts/react-roblox';
 import { Players } from '@rbxts/services';
@@ -9,12 +9,20 @@ const playerGui = Players.LocalPlayer.FindFirstChild('PlayerGui') as PlayerGui;
 
 export default class App {
   render() {
-    return <Sheet Size={new UDim2(0, 150, 0, 150)} />;
+    return (
+      <screengui>
+        <Sheet
+          HorizontalAlignment={Enum.HorizontalAlignment.Center}
+          Size={new UDim2(0, 300, 0, 150)}
+          VerticalAlignment={Enum.VerticalAlignment.Center}
+        >
+          <Typography Text="Hello world!" />
+        </Sheet>
+      </screengui>
+    );
   }
 
   constructor(root: ReactRoblox.Root) {
-    const rendered = this.render();
-
-    root.render(createPortal(rendered, playerGui));
+    root.render(createPortal(this.render(), playerGui));
   }
 }
